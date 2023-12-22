45365e004a9bc3b6eee34f3c9531c50091f32771
wangj <wangjie200010@gmail.com>
2023-12-22 14:46:02 +0000

    Configuring Springdoc Maven Plugin

--- system-demo/src/main/java/com/example/parcel_delivery_systembackendentry/controller/LogController.java
+++ system-demo/src/main/java/com/example/parcel_delivery_systembackendentry/controller/LogController.java
@@ -3,8 +3,14 @@ package com.example.parcel_delivery_systembackendentry.controller;
 import com.example.parcel_delivery_systembackendentry.common.Result;
 import com.example.parcel_delivery_systembackendentry.entity.ParcelTrack;
 import com.example.parcel_delivery_systembackendentry.service.ParcelTrackService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
 import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /*provide basic functions here
  * more functions to add along with development
@@ -15,13 +21,23 @@ public class LogController {
     @Resource
     private ParcelTrackService parcelTrackService;
 
+    @Operation(description = "receive a new Parcel")
     @PostMapping(value = "/newTrail")
+    @Parameters(value = {@Parameter (description = "newParcel")})
     public int newTrail(@RequestBody ParcelTrack data) {
         return 0;
     }
 
+    @Operation(description = "update a parcelTrack")
+    @PutMapping("/updateTrack")
+    @Parameters(value = {@Parameter(description = "updated ParcelTrack")})
+    public int updateTrack(@RequestBody ParcelTrack parcelTrack){
+        return 0;
+    }
+
+    @Operation(description = "get parcelTracks of a receiver")
     @GetMapping(value = "/getReceiverTrail")
-    public Result<Object> newTrail(@RequestParam int userId) {
+    public List<ParcelTrack> newTrail(@RequestParam int userId) {
         return null;
     }
 
