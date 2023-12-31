260191107e73174a1ec8d83a6ccb59aa209ef8d8
Yuhong He <71640320+Theodore-Ho@users.noreply.github.com>
2023-12-31 13:09:16 +0000

    create root access on entry service

--- Entry/src/test/java/com/example/entry/EntryApplicationTests.java
+++ Entry/src/test/java/com/example/entry/EntryApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ParcelDeliverySystemBackendEntryApplicationTests {
+class EntryApplicationTests {
 
     @Test
     void contextLoads() {
