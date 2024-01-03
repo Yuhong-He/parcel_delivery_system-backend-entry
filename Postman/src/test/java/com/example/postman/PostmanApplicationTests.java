8029af5f60c78de9f911f76069aafe2699db26e9
wangj <wangjie200010@gmail.com>
2024-01-03 00:00:20 +0000

    some changes

--- Postman/src/test/java/com/example/postman/PostmanApplicationTests.java
+++ Postman/src/test/java/com/example/postman/PostmanApplicationTests.java
@@ -1,11 +1,7 @@
 package com.example.postman;
 
-import com.example.postman.dto.ParcelTrackWithParcelID;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 @SpringBootTest
 class PostmanApplicationTests {
@@ -13,21 +9,21 @@ class PostmanApplicationTests {
     void contextLoads() {
     }
 
-    @Test
-    void MQTest() {
-
-        try {
-            String desc = "Test";
-            LocalDateTime currentDateTime = LocalDateTime.now();
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-            String formattedDateTime = currentDateTime.format(formatter);
-            ParcelTrackWithParcelID parcel = new ParcelTrackWithParcelID("014da20d-867a-4c2c-a1a8-10b9360abce1", desc, 3, false, 0, formattedDateTime);
-//            MQ.sendToDatabase(parcel);
-
-        } catch (Exception e) {
-            System.out.println("Exception:" + e);
-            e.printStackTrace();
-        }
-    }
+//    @Test
+//    void MQTest() {
+//
+//        try {
+//            String desc = "Test";
+//            LocalDateTime currentDateTime = LocalDateTime.now();
+//            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+//            String formattedDateTime = currentDateTime.format(formatter);
+//            ParcelTrackWithParcelID parcel = new ParcelTrackWithParcelID("014da20d-867a-4c2c-a1a8-10b9360abce1", desc, 3, false, 0, formattedDateTime);
+////            MQ.sendToDatabase(parcel);
+//
+//        } catch (Exception e) {
+//            System.out.println("Exception:" + e);
+//            e.printStackTrace();
+//        }
+//    }
 
 }
